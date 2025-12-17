a496079287d8e4da595d948a24635be58c493812
Fernando Aires de Oliveira <fernando.aires.oliveira@gmail.com>
2025-12-17 00:25:53 +0000

    adiciona pagina inicial Pagina corrigida

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Obrigado, meu amor, por ser meu porto seguro, minha força nos dias difíceis e minha alegria em todos os momentos; seu amor me sustenta, me inspira e dá sentido à minha vida.</h1>
+}
+
+export default Home; 
