20bc5001478f112b4fd12af0edaf4c3cac2fd4c6
Fernando Aires de Oliveira <fernando.aires.oliveira@gmail.com>
2025-12-18 00:35:20 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Obrigado, meu amor, por ser meu porto seguro, minha força nos dias difíceis e minha alegria em todos os momentos; seu amor me sustenta, me inspira e dá sentido à minha vida.</h1>
+    return <h1>Tina, eu te amo. Se você me ama, dá uma risadinha!</h1>
 }
 
 export default Home; 
